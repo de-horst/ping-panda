63779cc113fac2894c733799850b5532cc983997
Denise Horst <denisef.horst@gmail.com>
2024-11-01 10:28:24 -0300

    02 Navbar with auth

--- src/app/(landing)/page.tsx
+++ src/app/(landing)/page.tsx
@@ -1,7 +1,7 @@
-import { Heading } from "./components/heading"
-import { MaxWidthWrapper } from "./components/max-width-wrapper"
 import { Check } from "lucide-react"
-import { ShinyButton } from "./components/shiny-button"
+import { Heading } from "@/components/heading"
+import { MaxWidthWrapper } from "@/components/max-width-wrapper"
+import { ShinyButton } from "@/components/shiny-button"
 
 const Page = () => {
   return (
@@ -42,12 +42,28 @@ const Page = () => {
             </ul>
 
             <div className="w-full max-w-80">
-              <ShinyButton href="/sign-up" className="relative z-10 h-14 w-full text-base shadow-lg transition-shadow duration-300 hover:shadow-xl">Start For Free Today</ShinyButton>
+              <ShinyButton
+                href="/sign-up"
+                className="relative z-10 h-14 w-full text-base shadow-lg transition-shadow duration-300 hover:shadow-xl"
+              >
+                Start For Free Today
+              </ShinyButton>
             </div>
           </div>
         </MaxWidthWrapper>
       </section>
-      <section className=""></section>
+
+      <section className="relative bg-brand-25 pb-4">
+        <div className="absolute inset-x-0 bottom-24 top-24 bg-brand-700" />
+        <div className="relative mx-auto">
+          <MaxWidthWrapper className="relative">
+            <div className="-m-2 rounded-xl bg-gray-900/5 p-2 ring-1 ring-inset ring-gray-900/10 lg:-m-4 lg:rounded-2xl lg:p-4">
+              test
+            </div>
+          </MaxWidthWrapper>
+        </div>
+      </section>
+
       <section className=""></section>
       <section className=""></section>
     </>
